77f7a7fa56c7a866355a3b306444493b118f9614
gouzhun <gouzhun@libii.com>
2020-06-10 22:50:20 +0800

    add BashCommandExecutor

---
+++ BashCommandExecutor/BashCommandExecutor.cs
@@ -0,0 +1,56 @@
+﻿#if UNITY_EDITOR
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BashCommandExecutor
+{
+
+    [MenuItem("Suntabu/BashExecutorTest")]
+    public static void TestExecute()
+    {
+        Execute(new []{"git --version","pwd","ls -al"});
+    }
+    
+    
+    public static void Execute(string[] commands)
+    {
+        System.Diagnostics.Process process = new System.Diagnostics.Process();
+        System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo
+        {
+            RedirectStandardError = true,
+            RedirectStandardOutput = true,
+            RedirectStandardInput = true,
+            UseShellExecute = false,
+            FileName = "/bin/bash",
+            WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal
+        };
+        process.StartInfo = startInfo;
+        process.Start();
+        using (var sw = process.StandardInput)
+        {
+            foreach (var command in commands)
+            {
+                sw.WriteLine(command);
+            }
+        }
+        
+        
+        process.WaitForExit();
+        int exitCode = process.ExitCode;
+        if (exitCode != 0)
+        {
+            Debug.LogError("Run prebuild.command Failed : " + exitCode + "  " +
+                           process.StandardError.ReadToEnd() + "\n");
+        }
+        else
+        {
+            Debug.Log(process.StandardOutput.ReadToEnd());
+        }
+
+    }
+}
+
+#endif
--- RuntimeDebug/RuntimeDebug.cs
+++ RuntimeDebug/RuntimeDebug.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityToolKit.Utils;
 
 namespace UnityEngine
@@ -17,10 +16,11 @@ namespace UnityEngine
             Left
         }
 
+        public static bool IsSomethingEnabled;
+
 
         private int areaInPixel;
         public float areaRatio = 0.1f;
-        public static bool IsSomethingEnabled = false;
 
         public CommandPosEnum[] CommandPositions;
         private int currentInx;
@@ -33,18 +33,15 @@ namespace UnityEngine
 
         private void Update()
         {
-            if (CommandPositions.IsEmpty())
-            {
-                return;
-            }
-            
+            if (CommandPositions.IsEmpty()) return;
+
             if (currentInx >= CommandPositions.Length)
             {
                 currentInx = 0;
                 IsSomethingEnabled = !IsSomethingEnabled;
                 Debug.LogWarning("Succeed ---> enabled " + IsSomethingEnabled);
             }
-            
+
             if (Input.GetMouseButtonDown(0))
             {
                 if (ValidateClickPos(CommandPositions[currentInx]))
@@ -68,34 +65,20 @@ namespace UnityEngine
             {
                 case CommandPosEnum.LeftTop:
                     return Vector2.Distance(p, new Vector2(0, Screen.height)) <= areaInPixel;
-                    break;
                 case CommandPosEnum.Top:
                     return Vector2.Distance(p, new Vector2(Screen.width * 0.5f, Screen.height)) <= areaInPixel;
-                    break;
                 case CommandPosEnum.RightTop:
                     return Vector2.Distance(p, new Vector2(Screen.width, Screen.height)) <= areaInPixel;
-
-                    break;
                 case CommandPosEnum.Right:
                     return Vector2.Distance(p, new Vector2(Screen.width, Screen.height * 0.5f)) <= areaInPixel;
-
-                    break;
                 case CommandPosEnum.RightBottom:
                     return Vector2.Distance(p, new Vector2(Screen.width, 0)) <= areaInPixel;
-
-                    break;
                 case CommandPosEnum.Bottom:
                     return Vector2.Distance(p, new Vector2(Screen.width * .5f, 0)) <= areaInPixel;
-
-                    break;
                 case CommandPosEnum.LeftBottom:
                     return Vector2.Distance(p, new Vector2(0, 0)) <= areaInPixel;
-
-                    break;
                 case CommandPosEnum.Left:
                     return Vector2.Distance(p, new Vector2(0, Screen.height * 0.5f)) <= areaInPixel;
-
-                    break;
             }
 
 
